4175f773a8ade6ebc70ac859f5e9e4d3d818a905
kuhanE <55750899+kuhanE@users.noreply.github.com>
2019-09-25 01:49:17 +0800

    Test Comment at Line 28
    Try to comment

--- Purchasing System/PurchaseOrder/views.py
+++ Purchasing System/PurchaseOrder/views.py
@@ -25,7 +25,7 @@ from django.contrib.auth.models import User
 from django.core.mail import send_mail
 from django.conf import settings
 
-
+#Testing Comment
 @login_required
 def purchaseorderform(request):
     context = {
